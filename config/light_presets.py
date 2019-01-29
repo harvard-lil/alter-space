4f64e82245982dce43ad871ef3b8b74890c9906c
anastasia <anastasia.aizman@gmail.com>
2019-01-29 15:24:13 -0500

    move - presets into own files for sanity

---
+++ config/light_presets.py
@@ -0,0 +1,86 @@
+presets = {
+    "relax": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+                "duration": 600
+            },
+            {
+                "selector": "id:|8-15",
+                "brightness": 0.9,
+                "hue": 300,
+                "kelvin": 3500,
+            }
+        ],
+    },
+    "read": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+                "duration": 600
+            },
+        ]
+    },
+    "meditate": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+                "duration": 600
+            },
+        ]
+    },
+    "focus": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+            },
+        ]
+    },
+    "create": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+                "duration": 600
+            },
+        ]
+    },
+    "wyrd": {
+        "power": "on",
+        "states": [
+            {
+                "selector": "id:|0-7",
+                "brightness": 0.65,
+                "hue": 35,
+                "saturation": 0.37,
+                "kelvin": 3500,
+                "duration": 600
+            },
+        ]
+    },
+
+}
---
+++ config/sound_presets.py
@@ -0,0 +1,66 @@
+presets = {
+    "relax": {
+        "nature": [
+            "Bonfire.mp3",
+            "Forest_meadow.mp3"
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]},
+    "read": {
+        "nature": [
+            "Cafe.mp3",
+            "Winter_storm.mp3"
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]
+    },
+    "meditate": {
+        "nature": [
+            "Water_over_stones.mp3",
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]},
+    "wyrd": {
+        "nature": [
+            "White_noise.mp3"
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]
+    },
+    "focus": {
+        "nature": [
+            "Winter_storm.mp3"
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]},
+    "create": {
+        "nature": [
+
+        ],
+        "urban": [
+
+        ],
+        "abstract": [
+
+        ]},
+}
