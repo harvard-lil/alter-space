8691a2f4c45e79ab5a77243070881574419d82f7
anastasia <anastasia.aizman@gmail.com>
2019-02-12 17:14:46 -0500

    add - setting lights and tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,15 @@
+# from backend.views import backend_app
+from flask import url_for
+from backend.views import *
+
+
+# def test_some_endpoint(client):
+#     assert client.post(url_for('some_endpoint'),
+#            data=json.dumps(dict(some_attr='some_value')),
+#            content_type='application/json').status_code == 200
+
 def test_homepage(client):
-    response = client.get('/')
+    response = client.get(url_for('backend.index'))
     assert response.status_code == 200
 
 
@@ -11,3 +21,14 @@ def test_sounds(client):
     assert 'nature' in results
     for song in results['nature']:
         assert ".mp3" in song or ".wav" in song
+
+
+def test_activity(client):
+    response = client.get('/activity/relax')
+    assert response.status_code == 200
+    assert response.is_json
+    result = response.get_json()
+    assert "light" in result.keys()
+    assert "sound" in result.keys()
+
+    sound = result['sound']
