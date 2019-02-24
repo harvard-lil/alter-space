cfadb03674a1bb2dd11eeaf5cd40fe4979a17e9e
anastasia <anastasia.aizman@gmail.com>
2019-02-24 11:02:21 -0500

    update - svgs should show up correctly in chrome now

--- frontend/src/components/icons/arrow-up.js
+++ frontend/src/components/icons/arrow-up.js
@@ -5,6 +5,6 @@ icon.register({
     width: 16,
     height: 16,
     viewBox: '0 0 44.16 31.47',
-    data: '<g data-name="Layer 2"><g data-name="Layer 1"><path pid="0" _fill="#fff" d="M41.32 23.11l.72-.71-20.6-20.95L2.23 22.4l-.8.68H0v8.39h44.16v-8.36h-2.84z"/><path pid="1" _fill="#339" d="M41.33 23.1L21.47 2.9 2.97 23.08H0L21.42 0l22.74 23.11-2.83-.01z"/></g></g>'
+    data: '<g data-name="Layer 2"><g data-name="Layer 1"><path pid="0" _fill="#fff" d="M41.32 23.11l.01-.01L21.47 2.9 2.97 23.08H0v8.39h44.16v-8.36h-2.84z"/><path pid="1" _fill="#339" d="M21.42 0L0 23.08H2.97L21.47 2.9l19.86 20.2 2.83.01L21.42 0z"/></g></g>'
   }
 })
