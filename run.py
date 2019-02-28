09e15046bdfd97e40486a54073f4f09ac9caf3de
anastasia <anastasia.aizman@gmail.com>
2019-02-28 00:04:32 -0500

    remove - left over commented out lines

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ import os
 from backend import create_app
 
 if __name__ == "__main__":
-    # fh=open("/var/run/alterspace/flask.pid", 'w')
-    # fh.write(str(os.getpid()))
-    # fh.close()
     app = create_app()
     app.run(threaded=True)
 
