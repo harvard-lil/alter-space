a139787ff36d44eb5680d179df26d0cfe9a5ca93
anastasia <anastasia.aizman@gmail.com>
2019-06-06 21:49:24 -0400

    add - ios sound presets

---
+++ config/sound_presets_ios.py
@@ -0,0 +1,44 @@
+presets = {
+    "relax": {
+        "nature": {},
+        "urban": {},
+        "abstract": {
+            "Relax.mp3": 80,
+        }},
+    "read": {
+        "nature": {},
+        "urban": {},
+        "abstract": {
+            "Read.mp3": 80,
+        }
+    },
+    "meditate": {
+        "nature": {},
+        "urban": {},
+        "abstract": {
+            "Meditate.mp3": 80,
+        }},
+    "wyrd": {
+        "nature": {},
+        "urban": {
+            "Outside_cafe.mp3": 80,
+        },
+        "abstract": {
+            "Be Weird.mp3": 80,
+        }
+    },
+    "focus": {
+        "nature": {},
+        "urban": {},
+        "abstract": {
+            "Focus.mp3": 80
+        }},
+    "create": {
+        "nature": {
+            "Forest_meadow.mp3": 20
+        },
+        "urban": {},
+        "abstract": {
+            "Create.mp3": 80
+        }},
+}
